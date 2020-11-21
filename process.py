fdf0638f62b84e6cbdbc7e53e63dab09655e7de9
VictorBravo9er <vikramjitborah@gmail.com>
2020-11-21 19:38:25 +0530

    done as much

--- process.py
+++ process.py
@@ -3,17 +3,34 @@
 from os import stat
 import numpy as np
 
+
 class ProcessCircle:
     """Class for circle processing."""
 
     def __init__(self) -> None:
         """Construct new Processor."""
         super().__init__()
-        self.solution:float
+        self.state:list = []
+        self.solution:dict = {}
+
+    def reset(self):
+        """Reset for further processind on different data-set."""
+        self.state.clear()
+        self.solution.clear()
 
     def streamProcess(self, datum):
         """Process one piece of data at a Point."""
+        try:
+            p1, p2 = self.state[0:2]
+        except(ValueError):
+            self.state.append(datum)
+            if len(self.state) == 2:
+                p1, p2 = self.state[0:2]
+                self.solution["centre"] = ((p1[0] + p2[0])/2, (p1[1] + p2[1])/2)
+                self.solution["radius"] = ProcessCircle.distance(p1,p2)
+            return
         pass
+
 
     @staticmethod
     def reader(file):
@@ -26,9 +43,14 @@ class ProcessCircle:
                     yield(temp)
 
     @staticmethod
-    def process(dataSrc):
+    def process(dataSrcName):
         """Process data provided at once."""
         processor = ProcessCircle()
-        for data in ProcessCircle.reader(dataSrc):
+        for data in ProcessCircle.reader(dataSrcName):
             processor.streamProcess(data)
         return processor.solution
+
+    @staticmethod
+    def distance(thisPoint, thatPoint):
+        """Return distance between two points."""
+        return ( ((thisPoint[1] - thatPoint[1]) ** 2) + ((thisPoint[0] - thatPoint[0]) ** 2) ) * 0.5
